b3234c43023061876f122f1e62892cac20b08432
Parzi41 <kolyatishkevich@gmail.com>
2023-10-30 18:08:34 +0200

    Created class StringCalculator and finished step 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        StringCalculator calculator = new StringCalculator();
+        System.out.println(calculator.add(""));
+        System.out.println(calculator.add("1"));
+        System.out.println(calculator.add("1,2"));
     }
 }
