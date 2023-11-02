e63a8a5868dad04f241bbe11ecf814b97599fb4b
Parzi41 <kroshkabubon@gmail.com>
2023-11-02 21:31:05 +0200

    Lab 2 step 2 Done

--- Laboratory2/src/main/java/org/example/Main.java
+++ Laboratory2/src/main/java/org/example/Main.java
@@ -2,6 +2,18 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        Matrix emptyMatrix = new Matrix(2, 2);
+        emptyMatrix.printMatrix();
 
+        int[][] values = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        Matrix matrixFromValues = new Matrix(values);
+        matrixFromValues.printMatrix();
+
+        Matrix matrixCopy = new Matrix(matrixFromValues);
+        matrixCopy.printMatrix();
     }
 }
--- Laboratory2/src/main/java/org/example/Matrix.java
+++ Laboratory2/src/main/java/org/example/Matrix.java
@@ -4,4 +4,69 @@ public class Matrix {
     private int rows;
     private int columns;
     private int[][] data;
+
+    public Matrix(int rows, int columns) {
+        if (rows <= 0 || columns <= 0) {
+            throw new IllegalArgumentException("Colums and rows must be bigger than 0");
+        }
+
+        this.rows = rows;
+        this.columns = columns;
+        this.data = new int[rows][columns];
+    }
+
+    public Matrix(int[][] values) {
+        if (values == null || values.length == 0 || values[0].length == 0) {
+            throw new IllegalArgumentException("Incorrect data");
+        }
+
+        this.rows = values.length;
+        this.columns = values[0].length;
+        this.data = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            if (values[i].length != columns) {
+                throw new IllegalArgumentException("Matrix rows size is not equal");
+            }
+            for (int j = 0; j < columns; j++) {
+                this.data[i][j] = values[i][j];
+            }
+        }
+    }
+
+    public Matrix(Matrix other) {
+        this.rows = other.getRows();
+        this.columns = other.getColumns();
+        this.data = new int[rows][columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                this.data[i][j] = other.getElement(i, j);
+            }
+        }
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public int getElement(int row, int column) {
+        if (row < 0 || row >= rows || column < 0 || column >= columns) {
+            throw new IllegalArgumentException("Wrong matrix cords");
+        }
+        return data[row][column];
+    }
+
+    public void printMatrix() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
